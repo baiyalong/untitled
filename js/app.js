daaf072069a4872ea99ffef344bc44f44a2ba28d
baiyalong <baiyalong@live.cn>
2014-10-13 13:35:17 +0800

    test

--- js/app.js
+++ js/app.js
@@ -5,4 +5,6 @@
 
 var app = {};
 
-
+app.config = {
+    target: "#app-content"
+}
--- js/app.terminal.js
+++ js/app.terminal.js
@@ -5,11 +5,31 @@
 
 app.terminal = {
     init: function () {
+        app.grid.init(this.config);
+
 
     }
 };
 
 
 app.terminal.config = {
+    refresh: {
+        trigger: "#app-terminal-refresh"
+    },
+    remove: {},
+    get: {
+        trigger: "",
+        url: "/api/terminal/",
+        target: "#app-terminal-table",
+        thead: ["标识", "名称", "描述", "请求", "操作"],
+        tbody: {
+            callback: function (index, element) {
+                var tr;
+                tr += "<td>" + element.id + "</td>";
 
+
+                return tr;
+            }
+        }
+    }
 };
