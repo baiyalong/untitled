8e9a67fd021685dd2806db83145740ba096b13f5
baiyalong <baiyalong@live.cn>
2014-10-13 11:15:59 +0800

    ajax

--- js/app.ajax.js
+++ js/app.ajax.js
@@ -1,3 +1,41 @@
 /**
  * Created by yalong on 2014/10/11.
  */
+"use strict";
+
+app.ajax = {
+    get: function (config) {
+        $.get(config.url, function (data, status, xhr) {
+            status == "success" ? config.callback(data) : alert(status + data);
+        });
+    },
+    post: function (config) {
+        $.post(config.url, config.data, function (data, status, xhr) {
+            status == "success" ? config.callback(data) : alert(status + data);
+        });
+    },
+    put: function (config) {
+        $.ajax({
+            url: config.url,
+            type: "PUT",
+            success: function (data, status, xhr) {
+                config.callback(data);
+            },
+            error: function (xhr, status, error) {
+                alert(status + error);
+            }
+        });
+    },
+    remove: function (config) {
+        $.ajax({
+            url: config.url,
+            type: "DELETE",
+            success: function (data, status, xhr) {
+                config.callback(data);
+            },
+            error: function (xhr, status, error) {
+                alert(status + error);
+            }
+        });
+    }
+}
---
+++ js/app.grid.js
@@ -0,0 +1,19 @@
+/**
+ * Created by yalong on 2014/10/11.
+ */
+"use strict";
+
+app.grid = {
+    get: function () {
+
+    },
+    post: function () {
+
+    },
+    put: function () {
+
+    },
+    remove: function () {
+
+    }
+}
